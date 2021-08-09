9cbaf49b44527a41a5c99fc68b548db107f7a04a
seungyong <44765636+seungyong@users.noreply.github.com>
2021-08-09 16:49:59 +0900

    검색 미완성

--- Apache24/htdocs/js/commons/Header.js
+++ Apache24/htdocs/js/commons/Header.js
@@ -7,8 +7,64 @@ const headerEle = document.querySelector('.header');
 /* Search */
 const searchInputEle = document.querySelector('.search-input');
 const searchUnderlineEle = document.querySelector('.underline');
+const historyBoxEle = document.querySelector('.history-box');
+const allDeleteEle = document.querySelector('.all-delete');
 
-/* Dynamic Header */
+function addHistory() {
+    historyBoxEle.innerHTML = '';
+
+    // Array
+    let search = window.localStorage.search;
+
+    if (search === undefined) return;
+
+    let history = search.split(',');
+    
+    if (history.length > 8) {
+        window.localStorage.search = history.splice(0,8);
+        history = history.splice(0,8);
+    }
+
+    history.forEach((e) => {
+        let historyEle = document.createElement('div');
+        let historyNameEle = document.createElement('div');
+        let historyDeleteBoxEle = document.createElement('div');
+        let historyDeleteImgEle = document.createElement('img');
+
+        historyEle.className = 'history';
+        historyNameEle.className = 'history-name';
+        historyDeleteBoxEle.className = 'history-delete-box';
+        historyDeleteImgEle.className = 'history-delete-img';
+        historyDeleteImgEle.src = '../../assets/img/delete.png';
+
+        historyNameEle.innerText = e;
+
+        historyDeleteBoxEle.appendChild(historyDeleteImgEle);
+        historyEle.appendChild(historyNameEle);
+        historyEle.appendChild(historyDeleteBoxEle);
+
+        historyBoxEle.appendChild(historyEle);
+    });
+
+    document.body.addEventListener('click', (event) => {
+        if(event.target === historyBoxEle || event.target === searchInputEle) return;
+
+        historyBoxEle.innerHTML = '';
+        historyBoxEle.classList.add('none');
+        document.body.removeEventListener('click', () => {});
+    });
+
+    historyBoxEle.classList.remove('none');
+}
+
+// Search All Delete
+allDeleteEle.addEventListener('click', () => {
+    historyBoxEle.innerHTML = '';
+    window.localStorage.clear();
+    historyBoxEle.classList.add('none');
+});
+
+// Dynamic Header
 document.addEventListener('scroll', () => {
     scrollY = (window.pageYOffset || docEle.scrollTop)  - (docEle.clientTop || 0);
 
@@ -36,8 +92,12 @@ searchInputEle.addEventListener('mouseout', () => {
 
 searchInputEle.addEventListener('focus', () => {
     searchUnderlineEle.classList.add('--clicked');
+
+    addHistory();
 });
 
 searchInputEle.addEventListener('blur', () => {
     searchUnderlineEle.classList.remove('--clicked');
 });
+
+// historyBoxEle.classList.add('none');
